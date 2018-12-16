1ab053205c0e9e023477e1a7c1140d3db65cd1ad
Jose Medrano <jose.medrano@xamarin.com>
2018-12-16 03:38:32 +0100

    Adds a WinForms example project base

---
+++ FigmaSharp/IFigmaFile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace FigmaSharp
+{
+    public interface IFigmaFile
+    {
+        List<IImageViewWrapper> FigmaImages { get; }
+        IFigmaDocumentContainer Document { get; }
+
+        void Initialize();
+
+        void Reload(bool includeImages = false);
+
+        void ReloadImages();
+    }
+}
