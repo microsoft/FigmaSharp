dc067065126a1db767627890291317ec0d49f789
Jose Medrano <jose.medrano@xamarin.com>
2018-12-16 03:38:32 +0100

    Adds a WinForms example project base

--- ExampleFigmaWinForms/Form1.Designer.cs
+++ ExampleFigmaWinForms/Form1.Designer.cs
@@ -27,10 +27,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container ();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size (800, 450);
+            this.ClientSize = new System.Drawing.Size(876, 556);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
