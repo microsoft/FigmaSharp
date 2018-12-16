c4e62358584c8c854a3b0896f3bd1b4e9bcf0ce2
Jose Medrano <josmed@microsoft.com>
2018-12-16 19:53:58 +0100

    [iOS] Includes basic ViewWrappers

---
+++ FigmaSharp.iOS/FigmaApplication.cs
@@ -0,0 +1,16 @@
+﻿namespace FigmaSharp
+{
+    public class FigmaApplication
+    {
+        private FigmaApplication()
+        {
+        }
+
+        public static void Init(string token)
+        {
+            //Figma initialization
+            var applicationDelegate = new FigmaDelegate();
+            AppContext.Current.Configuration(applicationDelegate, token);
+        }
+    }
+}
---
+++ FigmaSharp.iOS/FigmaDelegate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using FigmaSharp.Converters;
+using UIKit;
+
+namespace FigmaSharp
+{
+    public class FigmaDelegate : IFigmaDelegate
+    {
+        static readonly FigmaViewConverter[] figmaViewConverters = {
+            //new FigmaVectorViewConverter (),
+            //new FigmaFrameEntityConverter (),
+            //new FigmaTextConverter (),
+            //new FigmaVectorEntityConverter (),
+            //new FigmaRectangleVectorConverter (),
+            //new FigmaElipseConverter (),
+            //new FigmaLineConverter ()
+        };
+
+        public IImageWrapper GetImage (string url)
+        {
+            var image = new UIImage(url);
+            return new ImageWrapper(image);
+        }
+
+        public IImageWrapper GetImageFromManifest (Assembly assembly, string imageRef)
+        {
+            var assemblyImage = FigmaViewsHelper.GetManifestImageResource(assembly, string.Format("{0}.png", imageRef));
+            return new ImageWrapper (assemblyImage);
+        }
+
+        public IImageWrapper GetImageFromFilePath(string filePath)
+        {
+           var image = new UIImage(filePath);
+           return new ImageWrapper(image);
+        }
+
+        public IImageViewWrapper GetImageView(FigmaPaint figmaPaint)
+        {
+            return new ImageViewWrapper(new UIImageView())
+            {
+                 Data = figmaPaint
+            };
+       }
+
+        public FigmaViewConverter[] GetFigmaConverters()
+        {
+            return figmaViewConverters;
+        }
+    }
+}
