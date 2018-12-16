edd6ecbfbbb3108e875c5a77174868c2b727a983
Jose Medrano <jose.medrano@xamarin.com>
2018-12-16 03:19:31 +0100

    Adds a WinForms example project

---
+++ FigmaDocumentExporter.WinForms/Program.cs
@@ -0,0 +1,25 @@
+﻿using FigmaDocumentExporter.WinForms.Properties;
+using FigmaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FigmaDocumentExporter.WinForms
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            FigmaEnvirontment.SetAccessToken (Settings.Default.TOKEN);
+            Application.EnableVisualStyles ();
+            Application.SetCompatibleTextRenderingDefault (false);
+            Application.Run (new Form1 ());
+        }
+    }
+}
