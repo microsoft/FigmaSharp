5729af73e6c0f4ada87e114aacfd31af5f666afe
Jose Medrano <josmed@microsoft.com>
2018-12-16 19:53:58 +0100

    [iOS] Includes basic ViewWrappers

---
+++ FigmaSharp.iOS/FigmaViewsHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using UIKit;
+
+namespace FigmaSharp
+{
+    internal class FigmaViewsHelper
+    {
+        internal static UIImage GetManifestImageResource(Assembly assembly, string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
