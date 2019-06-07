eef22343dc4ca936353fd9bdc6f4bd1547ac061c
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-06-07 11:13:00 +0200

    Added initial version of FigmaSharp Xamarin.Forms (alpha version, still many pending parts)

---
+++ FigmaSharp.NativeControls/NativeControls.Forms/ButtonConverter.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Text;
+using FigmaSharp.NativeControls.Base;
+using Xamarin.Forms;
+
+namespace FigmaSharp.NativeControls
+{
+    public class ButtonConverter : ButtonConverterBase
+    {
+        public override IViewWrapper ConvertTo(FigmaNode currentNode, ProcessedNode parent)
+        {
+            var view = new Button();
+
+            view.Configure(currentNode);
+   
+            var keyValues = GetKeyValues(currentNode);
+            foreach (var key in keyValues)
+            {
+                if (key.Key == "type")
+                {
+                    continue;
+                }
+
+                if (key.Key == "enabled")
+                {
+                    view.IsEnabled = key.Value == "true";
+                }
+            }
+            if (currentNode is IFigmaDocumentContainer instance)
+            {
+                var figmaText = instance.children.OfType<FigmaText>().FirstOrDefault(s => s.name == "title");
+                if (figmaText != null)
+                {
+                    view.Opacity = figmaText.opacity;
+                    view.Font = figmaText.style.ToFont();
+                    view.Text = figmaText.characters;
+                }
+            }
+
+            return new ViewWrapper(view);
+        }
+
+        public override string ConvertToCode(FigmaNode currentNode, ProcessedNode parent)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ samples/basic-rendering/BasicRendering.Forms/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using ExampleFigma;
+using FigmaSharp;
+using Xamarin.Forms;
+
+namespace BasicRendering.Forms
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        const string FigmaFileName = "INSERT YOUR FIGMA FILE IDENTIFIER HERE";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var scrollViewWrapper = new ScrollViewWrapper(ContainerPanel);
+            var manager = new ExampleViewManager(scrollViewWrapper, FigmaFileName);
+            manager.Initialize();
+        }
+    }
+}
