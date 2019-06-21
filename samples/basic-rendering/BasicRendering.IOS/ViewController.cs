e76442a6aeec3c3b02ebfe71267854d15324f4ee
Jose Medrano <josmed@microsoft.com>
2019-06-21 21:15:48 +0200

    Adds iOS example

---
+++ samples/basic-rendering/BasicRendering.IOS/ViewController.cs
@@ -0,0 +1,37 @@
+﻿using ExampleFigma;
+using FigmaSharp.iOS;
+using Foundation;
+using System;
+using UIKit;
+
+namespace BasicRendering.IOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        ExampleViewManager manager;
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            View.Layer.BackgroundColor = UIColor.Black.CGColor;
+
+            var scrollViewWrapper = new ScrollViewWrapper(MainScrollView);
+            var contentView = new ViewWrapper(new UIView());
+
+            manager = new ExampleViewManager(scrollViewWrapper, contentView);
+            manager.Initialize();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ samples/basic-rendering/BasicRendering.IOS/ViewController.designer.cs
@@ -0,0 +1,29 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace BasicRendering.IOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIScrollView MainScrollView { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (MainScrollView != null) {
+                MainScrollView.Dispose ();
+                MainScrollView = null;
+            }
+        }
+    }
+}
