87a397e7709ee1387d2ba6b54f5caa415f504f63
Jose Medrano <josmed@microsoft.com>
2019-06-22 05:23:57 +0200

    [Wpf] Adds Local file example

---
+++ LocalFile.Wpf/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using FigmaSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LocalFile.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            FigmaApplication.Init("YOUR FIGMA TOKEN HERE");
+        }
+    }
+}
