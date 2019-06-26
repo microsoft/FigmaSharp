55474ee5ac619106fbdbbf96876c523e3f79beca
Hylke Bons <hi@planetpeanut.uk>
2019-06-26 20:46:49 +0200

    IOS -> iOS

---
+++ samples/local-file-rendering/LocalFile.iOS/ViewController.cs
@@ -0,0 +1,35 @@
+﻿using FigmaSharp.iOS;
+using Foundation;
+using LocalFile.Shared;
+using System;
+using UIKit;
+
+namespace LocalFile.IOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        DocumentExample documentExample;
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+
+            var converters = FigmaSharp.AppContext.Current.GetFigmaConverters();
+            var storyboard = new FigmaStoryboard(converters);
+
+            var scrollViewWrapper = new ScrollViewWrapper(ScrollView);
+            documentExample = new DocumentExample(scrollViewWrapper, storyboard);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ samples/local-file-rendering/LocalFile.iOS/ViewController.designer.cs
@@ -0,0 +1,29 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace LocalFile.IOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIScrollView ScrollView { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (ScrollView != null) {
+                ScrollView.Dispose ();
+                ScrollView = null;
+            }
+        }
+    }
+}
