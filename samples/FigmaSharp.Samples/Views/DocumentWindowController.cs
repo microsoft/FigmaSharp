6c15a514b2b32b0fb69401e6dcfd50d8d628a1e8
Hylke Bons <hi@planetpeanut.uk>
2019-07-11 16:03:19 +0200

    samples: Implement more logic

--- samples/FigmaSharp.Samples/Views/DocumentWindowController.cs
+++ samples/FigmaSharp.Samples/Views/DocumentWindowController.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 using AppKit;
 using CoreGraphics;
@@ -11,72 +12,166 @@ namespace FigmaSharp.Samples
 {
     public partial class DocumentWindowController : NSWindowController
     {
+        public static int WindowCount { get; private set; }
+        const int NEW_WINDOW_OFFSET = 38;
+
+
+        public string Token = "";
+
+        public string Link_ID = "";
+        public string Version_ID = "Current";
+        public string Page_ID = "Page 1";
+
+
+        // FileProvider etc.
+
+
         public DocumentWindowController(IntPtr handle) : base(handle)
         {
         }
 
 
-        public static int WindowCount { get; private set; }
+        public string Title {
+            get { return TitleTextField.StringValue; }
+            set { TitleTextField.StringValue = value; }
+        }
 
-        public string test;
 
+        public void LoadDocument(string token, string link_id)
+        {
+            Token = token;
+            Link_ID = link_id;
 
+            Load(version_id: "", page_id: "");
+        }
 
 
         public override void WindowDidLoad()
         {
-
-            PagePopUpButton.Menu.AddItem(new NSMenuItem("Page 1"));
-            PagePopUpButton.Menu.AddItem(new NSMenuItem("Page 2"));
-            PagePopUpButton.Menu.AddItem(new NSMenuItem("Page 3"));
-            PagePopUpButton.Menu.AddItem(new NSMenuItem("Page 4"));
-            PagePopUpButton.Menu.AddItem(new NSMenuItem("Page 5"));
-
-            PagePopUpButton.Activated += delegate {
-                Console.WriteLine(PagePopUpButton.SelectedItem.Title);
-            };
-
-
-
-
-
-            WindowCount++;
-
-            int offset = 38;
-
-            CGRect frame = Window.Frame;
-            frame.X += offset * WindowCount;
-            frame.Y -= offset * WindowCount;
-            Window.SetFrame (frame, display: true);
-
-
+            PositionWindow();
             base.WindowDidLoad();
         }
 
 
-        public void ConfigureDocument(string token, string link_id)
+        void Load(string version_id, string page_id)
         {
-            TitleTextField.StringValue = string.Format("Opening “{0}”…", link_id);
+            Title = string.Format("Opening “{0}”…", Link_ID);
 
-            (Window.ContentViewController as DocumentViewController).Token = token;
-            (Window.ContentViewController as DocumentViewController).Link_ID = link_id;
+            (Window.ContentViewController as DocumentViewController).ToggleSpinnerState(toggle_on: true);
+            RefreshButton.Enabled = false;
+
+            new Thread(() => {
+                Thread.Sleep(2000);
+
+                this.InvokeOnMainThread(() => {
+                    Title = Link_ID;
+
+                    UpdateVersionMenu();
+                    UpdatePagesPopupButton();
+
+                    RefreshButton.Enabled = true;
+                    PagePopUpButton.Enabled = true;
+
+                    (Window.ContentViewController as DocumentViewController).ToggleSpinnerState(toggle_on: false);
+
+                });
+
+            }).Start();
+        }
+
+
+        public void Reload()
+        {
+            Load(Link_ID, Token);
+        }
+
+
+        void UpdatePagesPopupButton()
+        {
+            PagePopUpButton.AddItem("1");
+            PagePopUpButton.Activated += delegate {
+                Console.WriteLine(PagePopUpButton.SelectedItem.Title);
+            };
+        }
+
+
+        void UpdateVersionMenu()
+        {
+            var menu = new VersionMenu();
+
+            menu.VersionSelected += delegate (string version_id) {
+                Load(version_id, null);
+            };
+
+            menu.AddItem("1", "FigmaSharp.Cocoa 0.0.1", DateTime.Now);
+            menu.AddItem("2", "FigmaSharp.Cocoa 0.0.2", DateTime.Now);
+            menu.AddItem("3", "FigmaSharp.Cocoa 0.0.3", DateTime.Now);
+            menu.AddItem("4", DateTime.Now);
+            menu.AddItem("5", DateTime.Now.AddDays(-7));
+            menu.AddItem("6", DateTime.Now.AddDays(-14));
+
+            menu.UseAsVersionsMenu();
         }
 
 
         partial void RefreshClicked(NSObject sender)
         {
-            if (MainToolbar.VisibleItems[1].Identifier != "RefreshSpinner") {
-                MainToolbar.InsertItem("RefreshSpinner", 1);
-               (MainToolbar.VisibleItems[1].View as NSProgressIndicator).StartAnimation(this);
-            
-            } else {
+            ToggleSpinnerState(toggle_on: true);
+            RefreshButton.Enabled = false;
+
+            new Thread(() => {
+                Thread.Sleep(2000);
+
+                this.InvokeOnMainThread(() => {
+                    RefreshButton.Enabled = true;
+                    ToggleSpinnerState(toggle_on: false);
+                });
+
+            }).Start();
+
+            // Reload();
+        }
+
+
+        void ToggleSpinnerState(bool toggle_on)
+        {
+            if (MainToolbar.VisibleItems[1].Identifier == "Spinner") {
                 (MainToolbar.VisibleItems[1].View as NSProgressIndicator).StopAnimation(this);
                 MainToolbar.RemoveItem(1);
             }
 
-           // RefreshSpinner.View.Hidden = true;
-           //RefreshSpinner.
-            (Window.ContentViewController as DocumentViewController).Reload();
+            if (toggle_on) {
+                MainToolbar.InsertItem("Spinner", 1);
+                (MainToolbar.VisibleItems[1].View as NSProgressIndicator).StartAnimation(this);
+            }
+        }
+
+
+        void PositionWindow()
+        {
+            WindowCount++;
+            CGRect frame = Window.Frame;
+
+            frame.X += NEW_WINDOW_OFFSET * WindowCount;
+            frame.Y -= NEW_WINDOW_OFFSET * WindowCount;
+
+            Window.SetFrame(frame, display: true);
+        }
+
+
+        void ShowError()
+        {
+            /*
+            var alert = new NSAlert()
+            {
+                AlertStyle = NSAlertStyle.Warning,
+                MessageText = "Could not open “DhOTs1gwx837ysnG3X6RZqZm”",
+                InformativeText = "Please check if your Figma Link and Personal Access Token are correct",
+            };
+
+            alert.AddButton("Close");
+            alert.BeginSheetForResponse(alert.Window, null);
+            */
         }
     }
 }
