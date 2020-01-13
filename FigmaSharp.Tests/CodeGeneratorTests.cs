bca135a696731843809936c8ee5202f53cc982db
Jose Medrano <josmed@microsoft.com>
2020-01-13 10:08:24 +0100

    Adds some test to code generator

---
+++ FigmaSharp.Tests/CodeGeneratorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+using FigmaSharp;
+
+namespace FigmaSharp.Tests
+{
+	[TestFixture]
+	public class CodeGeneratorTests
+	{
+        [Test]
+        public void PartialDesignerClassGenerationTest ()
+        {
+            var codeGenerator = new FigmaPartialDesignerClass ();
+            codeGenerator.Usings.Add ("AppKit"); 
+            codeGenerator.ClassName = "MyGeneratedCustomView";
+            codeGenerator.Namespace = "LocalFile.Cocoa";
+            var classCode = codeGenerator.Generate ();
+            Assert.IsNotNullOrEmpty (classCode);
+        }
+
+        [Test]
+        public void PublicPartialClassGenerationTest ()
+        {
+            var codeGenerator = new FigmaPublicPartialClass ();
+            codeGenerator.Usings.Add ("AppKit");
+            codeGenerator.ClassName = "MyGeneratedCustomView";
+            codeGenerator.Namespace = "LocalFile.Cocoa";
+            codeGenerator.BaseClass = "AppKit.NSView";
+            var classCode = codeGenerator.Generate ();
+            Assert.IsNotNullOrEmpty (classCode);
+        }
+    }
+}
