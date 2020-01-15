e6cd4fe2044320c9effdb431b8fa107d65c7227d
Jose Medrano <josmed@microsoft.com>
2020-01-15 17:43:42 +0100

    Implements ConvertToCode in ComboBoxConverter Fixes 1051616: Implement ConvertToCode from CheckBoxConverter

--- FigmaSharp.NativeControls/FigmaSharp.NativeControls.Cocoa/Converters/ComboBoxConverter.cs
+++ FigmaSharp.NativeControls/FigmaSharp.NativeControls.Cocoa/Converters/ComboBoxConverter.cs
@@ -28,10 +28,13 @@
 using AppKit;
 using FigmaSharp.NativeControls.Base;
 using System;
+using FigmaSharp.Cocoa;
 using FigmaSharp.Models;
+using System.Linq;
 using FigmaSharp.Views;
 using FigmaSharp.Services;
 using FigmaSharp.Views.Cocoa;
+using System.Text;
 
 namespace FigmaSharp.NativeControls.Cocoa
 {
@@ -65,7 +68,40 @@ namespace FigmaSharp.NativeControls.Cocoa
 
         public override string ConvertToCode(FigmaNode currentNode, FigmaCodeRendererService rendererService)
         {
-            return string.Empty;
+            var figmaInstance = (FigmaInstance)currentNode;
+
+            var builder = new StringBuilder ();
+            var name = FigmaSharp.Resources.Ids.Conversion.NameIdentifier;
+            builder.AppendLine ($"var {name} = new {typeof (NSComboBox).FullName}();");
+
+            builder.AppendLine (string.Format ("{0}.BezelStyle = {1};", name, NSBezelStyle.Rounded.GetFullName ()));
+            builder.AppendLine (string.Format ("{0}.Title = string.Empty;", name));
+
+            builder.Configure (name, currentNode);
+
+            var controlType = figmaInstance.ToControlType ();
+            switch (controlType) {
+                case NativeControlType.PopUpButtonSmall:
+                case NativeControlType.PopUpButtonSmallDark:
+                    builder.AppendLine (string.Format ("{0}.ControlSize = {1};", name, NSControlSize.Small.GetFullName ()));
+                    break;
+                case NativeControlType.PopUpButtonStandard:
+                case NativeControlType.PopUpButtonStandardDark:
+                    builder.AppendLine (string.Format ("{0}.ControlSize = {1};", name, NSControlSize.Regular.GetFullName ()));
+                    break;
+            }
+
+            var label = figmaInstance.children.OfType<FigmaText> ().FirstOrDefault ();
+
+            if (!string.IsNullOrEmpty (label?.characters)) {
+                builder.AppendLine (string.Format ("{0}.AddItem (\"{1}\");", name, label.characters));
+            }
+
+            if (controlType.ToString ().EndsWith ("Dark", StringComparison.Ordinal)) {
+                builder.AppendLine (string.Format ("{0}.Appearance = NSAppearance.GetAppearance ({1});", name, NSAppearance.NameDarkAqua.GetType ().FullName));
+            }
+
+            return builder.ToString ();
         }
     }
 }
