d5f44c5c79acc0b16481731b5eaac56cf75975f6
Jose Medrano <josmed@microsoft.com>
2020-01-16 17:20:15 +0100

    Adds some extensions methods to check figma directories

---
+++ FigmaSharp.Tools/MonoDevelop.Figma/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MonoDevelop.Ide.Gui.Pads.ProjectPad;
+using MonoDevelop.Projects;
+
+namespace MonoDevelop.Figma
+{
+	public static class Extensions
+	{
+		const string FigmaBundleDirectoryExtension = ".figmabundle";
+		const string FigmaBundlesDirectoryName = ".figmabundles";
+
+		public static bool IsDocumentDirectoryBundle (this ProjectFolder pr)
+		{
+			return pr.Path.Extension == FigmaBundleDirectoryExtension
+			&& pr.Parent is ProjectFolder figmaBundles
+			&& figmaBundles.Path.FileName == FigmaBundlesDirectoryName
+			&& figmaBundles.Parent is Project;
+		}
+
+		public static bool IsFigmaBundleDirectory (this ProjectFolder pr)
+		{
+			return pr.Path.FileName == FigmaBundlesDirectoryName && pr.Parent is Project;
+		}
+	}
+}
