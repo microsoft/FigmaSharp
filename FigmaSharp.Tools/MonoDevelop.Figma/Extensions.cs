7cd2638283149a84849a3596269b1b206f1454fc
Jose Medrano <josmed@microsoft.com>
2020-01-20 13:03:52 +0100

    Changes current menu hierarchy to a base Figma element

--- FigmaSharp.Tools/MonoDevelop.Figma/Extensions.cs
+++ FigmaSharp.Tools/MonoDevelop.Figma/Extensions.cs
@@ -7,19 +7,28 @@ namespace MonoDevelop.Figma
 	public static class Extensions
 	{
 		const string FigmaBundleDirectoryExtension = ".figmabundle";
-		const string FigmaBundlesDirectoryName = ".figmabundles";
+		const string FigmaBundlesDirectoryName = ".bundles";
+		const string FigmaDirectoryName = ".figma";
 
 		public static bool IsDocumentDirectoryBundle (this ProjectFolder pr)
 		{
 			return pr.Path.Extension == FigmaBundleDirectoryExtension
-			&& pr.Parent is ProjectFolder figmaBundles
-			&& figmaBundles.Path.FileName == FigmaBundlesDirectoryName
-			&& figmaBundles.Parent is Project;
+			&& pr.Parent is ProjectFolder figmaBundles && figmaBundles.Path.FileName == FigmaBundlesDirectoryName
+			&& figmaBundles.Parent is ProjectFolder figmaProject && figmaProject.Path.FileName == FigmaDirectoryName
+			&& figmaProject.Parent is Project;
+		}
+
+		public static bool IsFigmaDirectory (this ProjectFolder pr)
+		{
+			return pr.Path.Extension == FigmaDirectoryName
+			&& pr.Parent is Project;
 		}
 
 		public static bool IsFigmaBundleDirectory (this ProjectFolder pr)
 		{
-			return pr.Path.FileName == FigmaBundlesDirectoryName && pr.Parent is Project;
+			return pr.Path.FileName == FigmaBundlesDirectoryName
+				&& pr.Parent is ProjectFolder figmaProject && figmaProject.Path.FileName == FigmaDirectoryName
+				&& figmaProject.Parent is Project;
 		}
 	}
 }
