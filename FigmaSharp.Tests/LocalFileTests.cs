27e7d5f5a370607151b35e2c3b5519fed3bb2de6
Jose Medrano <josmed@microsoft.com>
2020-01-30 20:13:13 +0100

    [Tests] Adds some local file tests

---
+++ FigmaSharp.Tests/LocalFileTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace FigmaSharp.Tests
+{
+	[TestFixture]
+	public class LocalFileTests
+	{
+		const string fileExample = "test.json";
+		readonly string currentPath;
+		public LocalFileTests ()
+		{
+			currentPath = Path.GetDirectoryName (this.GetType ().Assembly.Location);
+		}
+
+		[Test]
+		public void LocalFileTest ()
+		{
+			var file = Path.Combine (currentPath, fileExample);
+			var data = File.ReadAllText (file);
+			var doc = FigmaApiHelper.GetFigmaResponseFromFileContent (data);
+			Assert.IsNotNull (file);
+		}
+	}
+}
