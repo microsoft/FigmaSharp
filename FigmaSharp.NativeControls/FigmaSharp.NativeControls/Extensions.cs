f3ac2920522b037e6a445cada4e4df7d44bfad86
Jose Medrano <josmed@microsoft.com>
2020-03-04 17:50:44 +0100

    Fixes accessibility to work as a11 node child Fixes #90

--- FigmaSharp.NativeControls/FigmaSharp.NativeControls/Extensions.cs
+++ FigmaSharp.NativeControls/FigmaSharp.NativeControls/Extensions.cs
@@ -37,9 +37,13 @@ namespace FigmaSharp
 {
     public static class Extensions
     {
-        const string a11yLabel = "a11y-label:\"";
-        const string a11yHelp = "a11y-help:\"";
-        const string a11yGroup = "a11y-group";
+        const string a11yLabel = "label:\"";
+        const string a11yHelp = "help:\"";
+        const string a11yRole = "role:\"";
+
+        const string a11yRoleGroup = "group";
+
+        const string a11yNodeName = "!a11y";
 
         static bool TrySearchParameter(FigmaNode node, string parameter, out string value)
         {
@@ -65,19 +69,48 @@ namespace FigmaSharp
             return false;
         }
 
+        public static bool TryGetPropertyValue (this FigmaNode node, string property, out string value)
+		{
+            if (node is IFigmaNodeContainer container)
+            {
+                foreach (var item in container.children) {
+                   if (TrySearchParameter (item, property, out value)) {
+                        return true;
+				   }
+                }
+            }
+            value = null;
+            return false;
+        }
+
         public static bool IsA11Group (this FigmaNode node)
         {
-            return (node.name.Contains(a11yGroup));
+            if (TryGetPropertyValue (node.GetA11Node(), a11yRole, out var value) && value == a11yRoleGroup) {
+                return true;
+            }
+            return false;
+        }
+
+        public static FigmaNode GetA11Node (this FigmaNode node)
+        {
+            return (node as IFigmaNodeContainer)?.children.FirstOrDefault (s => s.name == a11yNodeName);
         }
 
         public static bool TrySearchA11Label(this FigmaNode node, out string label)
         {
-            return TrySearchParameter (node, a11yLabel, out label);
+			if (TryGetPropertyValue(node.GetA11Node(), a11yLabel, out label)) {
+                return true;
+            }
+            return false;
         }
 
         public static bool TrySearchA11Help(this FigmaNode node, out string label)
         {
-            return TrySearchParameter(node, a11yHelp, out label);
+            if (TryGetPropertyValue(node.GetA11Node(), a11yHelp, out label))
+            {
+                return true;
+            }
+            return false;
         }
 
         public static bool IsFigmaImageViewNode (this FigmaNode node)
