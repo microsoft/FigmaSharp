c085982d4a5510e77f1ed8392e059a98899ec3c9
Hylke Bons <hi@planetpeanut.uk>
2020-05-14 00:41:48 +0200

    cocoa: Whitespace

--- FigmaSharp.NativeControls/FigmaSharp.NativeControls/NativeControlTypes.cs
+++ FigmaSharp.NativeControls/FigmaSharp.NativeControls/NativeControlTypes.cs
@@ -88,52 +88,52 @@ namespace FigmaSharp.NativeControls
             new List<(string name, NativeControlType nativeControlType, NativeControlVariant nativeControlVariant)>
         {
             // Buttons
-            ( "Button",            NativeControlType.Button,     NativeControlVariant.Regular),
-            ( "Button Small",      NativeControlType.Button,     NativeControlVariant.Small),
-            ( "Button/Help",       NativeControlType.ButtonHelp, NativeControlVariant.Regular),
-            ( "Button/Help Small", NativeControlType.ButtonHelp, NativeControlVariant.Small),
+            ("Button",            NativeControlType.Button,     NativeControlVariant.Regular),
+            ("Button Small",      NativeControlType.Button,     NativeControlVariant.Small),
+            ("Button/Help",       NativeControlType.ButtonHelp, NativeControlVariant.Regular),
+            ("Button/Help Small", NativeControlType.ButtonHelp, NativeControlVariant.Small),
 
 
             // Labels
-            ( "Label",                 NativeControlType.Label,          NativeControlVariant.Regular),
-            ( "Label Small",           NativeControlType.Label,          NativeControlVariant.Small),
-            ( "Label/Group",           NativeControlType.LabelGroup,     NativeControlVariant.Regular),
-            ( "Label/Group Small",     NativeControlType.LabelGroup,     NativeControlVariant.Small),
-            ( "Label/Header",          NativeControlType.LabelHeader,    NativeControlVariant.Regular),
-            ( "Label/Header Small",    NativeControlType.LabelHeader,    NativeControlVariant.Small),
-            ( "Label/Secondary",       NativeControlType.LabelSecondary, NativeControlVariant.Regular),
-            ( "Label/Secondary Small", NativeControlType.LabelSecondary, NativeControlVariant.Small),
+            ("Label",                 NativeControlType.Label,          NativeControlVariant.Regular),
+            ("Label Small",           NativeControlType.Label,          NativeControlVariant.Small),
+            ("Label/Group",           NativeControlType.LabelGroup,     NativeControlVariant.Regular),
+            ("Label/Group Small",     NativeControlType.LabelGroup,     NativeControlVariant.Small),
+            ("Label/Header",          NativeControlType.LabelHeader,    NativeControlVariant.Regular),
+            ("Label/Header Small",    NativeControlType.LabelHeader,    NativeControlVariant.Small),
+            ("Label/Secondary",       NativeControlType.LabelSecondary, NativeControlVariant.Regular),
+            ("Label/Secondary Small", NativeControlType.LabelSecondary, NativeControlVariant.Small),
 
 
             // TextFields
-            ( "TextField",       NativeControlType.TextField, NativeControlVariant.Regular),
-            ( "TextField Small", NativeControlType.TextField, NativeControlVariant.Small),
+            ("TextField",       NativeControlType.TextField, NativeControlVariant.Regular),
+            ("TextField Small", NativeControlType.TextField, NativeControlVariant.Small),
 
-            ( "Stepper",       NativeControlType.Stepper, NativeControlVariant.Regular),
-            ( "Stepper Small", NativeControlType.Stepper, NativeControlVariant.Small),
+            ("Stepper",       NativeControlType.Stepper, NativeControlVariant.Regular),
+            ("Stepper Small", NativeControlType.Stepper, NativeControlVariant.Small),
 
-            ( "TextView",       NativeControlType.TextView, NativeControlVariant.Regular),
-            ( "TextView Small", NativeControlType.TextView, NativeControlVariant.Small),
+            ("TextView",       NativeControlType.TextView, NativeControlVariant.Regular),
+            ("TextView Small", NativeControlType.TextView, NativeControlVariant.Small),
 
-            ( "SearchField",       NativeControlType.SearchField, NativeControlVariant.Regular),
-            ( "SearchField Small", NativeControlType.SearchField, NativeControlVariant.Small),
+            ("SearchField",       NativeControlType.SearchField, NativeControlVariant.Regular),
+            ("SearchField Small", NativeControlType.SearchField, NativeControlVariant.Small),
 
 
             // Selections
-            ( "Radio",       NativeControlType.Radio, NativeControlVariant.Regular),
-            ( "Radio Small", NativeControlType.Radio, NativeControlVariant.Small),
+            ("Radio",       NativeControlType.Radio, NativeControlVariant.Regular),
+            ("Radio Small", NativeControlType.Radio, NativeControlVariant.Small),
 
-            ( "Checkbox",       NativeControlType.CheckBox, NativeControlVariant.Regular),
-            ( "Checkbox Small", NativeControlType.CheckBox, NativeControlVariant.Small),
+            ("Checkbox",       NativeControlType.CheckBox, NativeControlVariant.Regular),
+            ("Checkbox Small", NativeControlType.CheckBox, NativeControlVariant.Small),
 
-            ( "PopUp Button",       NativeControlType.PopUp, NativeControlVariant.Regular),
-            ( "PopUp Button Small", NativeControlType.PopUp, NativeControlVariant.Small),
+            ("PopUp Button",       NativeControlType.PopUp, NativeControlVariant.Regular),
+            ("PopUp Button Small", NativeControlType.PopUp, NativeControlVariant.Small),
 
-            ( "PullDown Button",       NativeControlType.PullDown, NativeControlVariant.Regular),
-            ( "PullDown Button Small", NativeControlType.PullDown, NativeControlVariant.Small),
+            ("PullDown Button",       NativeControlType.PullDown, NativeControlVariant.Regular),
+            ("PullDown Button Small", NativeControlType.PullDown, NativeControlVariant.Small),
 
-            ( "ComboBox",       NativeControlType.ComboBox, NativeControlVariant.Regular),
-            ( "ComboBox Small", NativeControlType.ComboBox, NativeControlVariant.Small),
+            ("ComboBox",       NativeControlType.ComboBox, NativeControlVariant.Regular),
+            ("ComboBox Small", NativeControlType.ComboBox, NativeControlVariant.Small),
 
 
             // Status
@@ -144,9 +144,9 @@ namespace FigmaSharp.NativeControls
 
 
             // Containers
-            ( "TabView",        NativeControlType.TabView,        NativeControlVariant.Regular),
-            ( "DisclosureView", NativeControlType.DisclosureView, NativeControlVariant.Regular),
-            ( "Box",            NativeControlType.Box,            NativeControlVariant.Regular),
+            ("TabView",        NativeControlType.TabView,        NativeControlVariant.Regular),
+            ("DisclosureView", NativeControlType.DisclosureView, NativeControlVariant.Regular),
+            ("Box",            NativeControlType.Box,            NativeControlVariant.Regular),
 
 
             // Windows
