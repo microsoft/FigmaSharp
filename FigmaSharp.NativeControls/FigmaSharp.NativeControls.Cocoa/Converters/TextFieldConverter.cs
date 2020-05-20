132b45e2dfa24a78f89ec74089ff9f8ae3d950f3
Hylke Bons <hi@planetpeanut.uk>
2020-05-21 00:52:19 +0200

    cocoa: TextField

--- FigmaSharp.NativeControls/FigmaSharp.NativeControls.Cocoa/Converters/TextFieldConverter.cs
+++ FigmaSharp.NativeControls/FigmaSharp.NativeControls.Cocoa/Converters/TextFieldConverter.cs
@@ -1,30 +1,27 @@
-﻿/* 
- * CustomTextFieldConverter.cs
- * 
- * Author:
- *   Jose Medrano <josmed@microsoft.com>
- *
- * Copyright (C) 2018 Microsoft, Corp
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to permit
- * persons to whom the Software is furnished to do so, subject to the
- * following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
- * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+﻿// Authors:
+//   Jose Medrano <josmed@microsoft.com>
+//   Hylke Bons <hylbo@microsoft.com>
+//
+// Copyright (C) 2020 Microsoft, Corp
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 using System;
 using System.Linq;
@@ -40,95 +37,132 @@ using FigmaSharp.Views.Cocoa;
 
 namespace FigmaSharp.NativeControls.Cocoa
 {
-    public class TextFieldConverter : FigmaNativeControlConverter
+    public class TextFieldConverter : CocoaConverter
 	{
 		public override Type GetControlType(FigmaNode currentNode)
 		{
+			FigmaNode optionsGroup = currentNode.GetChildren()
+                .FirstOrDefault(s => s.name == "!options" && s.visible);
+
+			FigmaNode passwordNode = optionsGroup?.GetChildren()
+				.OfType<FigmaNode>()
+				.FirstOrDefault(s => s.name == ComponentString.PASSWORD && s.visible);
+
+			if (passwordNode != null)
+				return typeof(NSSecureTextField);
+
+
+			currentNode.TryGetNativeControlType(out var controlType);
+
+			if (controlType == NativeControlType.SearchField)
+				return typeof(NSSearchField);
+
 			return typeof(NSTextField);
 		}
 
 		public override bool CanConvert(FigmaNode currentNode)
 		{
-			return currentNode.TryGetNativeControlType(out var value) && (value == NativeControlType.TextField || value == NativeControlType.Filter);
+			currentNode.TryGetNativeControlType(out var controlType);
+
+			return controlType == NativeControlType.TextField ||
+				   controlType == NativeControlType.SearchField;
 		}
 
-		protected override IView OnConvertToView (FigmaNode currentNode, ProcessedNode parent, FigmaRendererService rendererService)
+
+		protected override IView OnConvertToView (FigmaNode currentNode, ProcessedNode parentNode, FigmaRendererService rendererService)
 		{
-			var figmaInstance = (FigmaFrameEntity) currentNode;
+			var textField = new NSTextField();
 
-			figmaInstance.TryGetNativeControlType (out var controlType);
-			ITextBox textBox = controlType == NativeControlType.Filter ? (ITextBox) new SearchBox() : new TextBox();
-			var view = (NSTextField)textBox.NativeObject;
+			var frame = (FigmaFrame) currentNode;
+			frame.TryGetNativeControlType(out var controlType);
+			frame.TryGetNativeControlVariant(out var controlVariant);
 
-			view.Configure (currentNode);
 
-			figmaInstance.TryGetNativeControlComponentType (out var controlComponentType);
-			switch (controlComponentType) {
-				default:
-					view.Font = NSFont.SystemFontOfSize(NSFont.SystemFontSize);
-					break;
-				case NativeControlComponentType.TextFieldSmall:
-				case NativeControlComponentType.TextFieldSmallDark:
-				case NativeControlComponentType.FilterSmall:
-				case NativeControlComponentType.FilterSmallDark:
-					view.ControlSize = NSControlSize.Small;
-					break;
-			}
-	
-			var texts = figmaInstance.children
-				.OfType<FigmaText> ();
+			if (controlType == NativeControlType.SearchField)
+				textField = new NSSearchField();
 
-			var text = texts.FirstOrDefault (s => s.name == "lbl" && s.visible);
-			if (text != null) {
-				textBox.Text = text.characters;
-				//view.Configure (text);
+
+			FigmaNode optionsGroup = frame.children.FirstOrDefault(s => s.name == "!options" && s.visible);
+
+			FigmaNode passwordNode = optionsGroup?.GetChildren()
+	            .OfType<FigmaNode>()
+	            .FirstOrDefault(s => s.name == ComponentString.PASSWORD && s.visible);
+
+			if (passwordNode != null)
+			{
+				textField = new NSSecureTextField();
+				textField.StringValue = "Password";
 			}
 
-			var placeholder = texts.FirstOrDefault (s => s.name == "placeholder");
-			if (placeholder != null && !placeholder.characters.Equals("Placeholder", StringComparison.InvariantCultureIgnoreCase))
-				view.PlaceholderString = placeholder.characters;
 
-			return textBox;
+			FigmaText placeholderText = optionsGroup?.GetChildren()
+				.OfType<FigmaText>()
+				.FirstOrDefault(s => s.name == ComponentString.PLACEHOLDER && s.visible);
+
+			if (placeholderText != null && !placeholderText.characters.Equals(ComponentString.PLACEHOLDER, StringComparison.InvariantCultureIgnoreCase))
+				textField.PlaceholderString = placeholderText.characters;
+
+
+
+			FigmaText text = frame.children
+				.OfType<FigmaText> ()
+                .FirstOrDefault (s => s.name == ComponentString.TITLE && s.visible);
+
+			if (text != null)
+			{
+				textField.Alignment = GetNSTextAlignment(text);
+				textField.StringValue = text.characters;
+			}
+
+			textField.ControlSize = GetNSControlSize(controlVariant);
+			textField.Font = GetNSFont(controlVariant);
+
+			return new View(textField);
 		}
+
 
 		protected override StringBuilder OnConvertToCode (FigmaCodeNode currentNode, FigmaCodeNode parentNode, FigmaCodeRendererService rendererService)
 		{
-			var instance = (FigmaFrameEntity)currentNode.Node;
-			var name = currentNode.Name;
+			var code = new StringBuilder();
+			string name = FigmaSharp.Resources.Ids.Conversion.NameIdentifier;
 
-			var builder = new StringBuilder ();
-			if (rendererService.NeedsRenderConstructor (currentNode, parentNode))
-				builder.WriteConstructor (name,  GetControlType(currentNode.Node), rendererService.NodeRendersVar(currentNode, parentNode));
+			var frame = (FigmaFrame)currentNode.Node;
+			currentNode.Node.TryGetNativeControlType(out NativeControlType controlType);
+			currentNode.Node.TryGetNativeControlVariant(out NativeControlVariant controlVariant);
 
-			builder.Configure (instance, name);
+			if (rendererService.NeedsRenderConstructor(currentNode, parentNode))
+				code.WriteConstructor(name, GetControlType(currentNode.Node), rendererService.NodeRendersVar(currentNode, parentNode));
 
-			instance.TryGetNativeControlComponentType(out var controlType);
-			switch (controlType)
-			{
-				case NativeControlComponentType.TextFieldStandard:
-				case NativeControlComponentType.TextFieldStandardDark:
-					builder.WriteEquality(currentNode.Name, nameof(NSButton.Font),
-						CodeGenerationHelpers.Font.SystemFontOfSize(CodeGenerationHelpers.Font.SystemFontSize));
-					break;
+			FigmaNode optionsGroup = frame.children.FirstOrDefault(s => s.name == "!options" && s.visible);
+
+			FigmaNode passwordNode = optionsGroup?.GetChildren()
+				.OfType<FigmaNode>()
+				.FirstOrDefault(s => s.name == ComponentString.PASSWORD && s.visible);
+
+			if (passwordNode != null)
+				code.WriteEquality(name, nameof(NSSecureTextField.StringValue), ComponentString.PASSWORD, inQuotes: true);
+
+			FigmaText placeholderText = optionsGroup?.GetChildren().
+				OfType<FigmaText>().
+				FirstOrDefault (s => s.name == ComponentString.PLACEHOLDER && s.visible);
+
+			if (placeholderText != null && !placeholderText.characters.Equals(ComponentString.PLACEHOLDER, StringComparison.InvariantCultureIgnoreCase)) {
+				var stringLabel = NativeControlHelper.GetTranslatableString(placeholderText.characters, rendererService.CurrentRendererOptions.TranslateLabels);
+				code.WriteEquality(name, nameof(NSTextField.PlaceholderString), stringLabel, true);
 			}
 
-			var texts = instance.children.OfType<FigmaText> ();
+			FigmaText text = frame.children.
+                OfType<FigmaText> ().
+                FirstOrDefault (s => s.name == ComponentString.TITLE && s.visible);
 
-			var figmaTextNode = texts.FirstOrDefault (s => s.name == "lbl" && s.visible);
+			if (text != null) {
+				code.WriteEquality(name, nameof(NSTextField.Font), GetNSFontName(controlVariant, text, withWeight: false));
 
-			if (figmaTextNode != null) {
-				var stringLabel = NativeControlHelper.GetTranslatableString(figmaTextNode.characters, rendererService.CurrentRendererOptions.TranslateLabels);
-				builder.WriteEquality (name, nameof (NSTextField.StringValue), stringLabel, inQuotes: !rendererService.CurrentRendererOptions.TranslateLabels);
-				//builder.Configure (figmaTextNode, name);
+				var stringLabel = NativeControlHelper.GetTranslatableString(text.characters, rendererService.CurrentRendererOptions.TranslateLabels);
+				code.WriteEquality (name, nameof (NSTextField.StringValue), stringLabel, inQuotes: !rendererService.CurrentRendererOptions.TranslateLabels);
 			}
 
-			var placeholderTextNode = texts.FirstOrDefault (s => s.name == "placeholder");
-			if (placeholderTextNode != null && !placeholderTextNode.characters.Equals("Placeholder", StringComparison.InvariantCultureIgnoreCase)) {
-				var stringLabel = NativeControlHelper.GetTranslatableString(placeholderTextNode.characters, rendererService.CurrentRendererOptions.TranslateLabels);
-				builder.WriteEquality(name, nameof(NSTextField.PlaceholderString), stringLabel, true);
-			}
-
-			return builder;
+			return code;
 		}
 	}
 }
