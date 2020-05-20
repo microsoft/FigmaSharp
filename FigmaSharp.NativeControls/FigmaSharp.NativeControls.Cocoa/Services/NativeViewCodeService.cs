6408c26c63b4dd4c357d8f17c6e002ce7dd9ddcf
Hylke Bons <hi@planetpeanut.uk>
2020-05-21 00:39:21 +0200

    cocoa: A few missed renames

--- FigmaSharp.NativeControls/FigmaSharp.NativeControls.Cocoa/Services/NativeViewCodeService.cs
+++ FigmaSharp.NativeControls/FigmaSharp.NativeControls.Cocoa/Services/NativeViewCodeService.cs
@@ -65,7 +65,7 @@ namespace FigmaSharp.Services
 				}
             }
 
-            if (node.Node.Parent is FigmaCanvas && node.Node is FigmaFrameEntity) {
+            if (node.Node.Parent is FigmaCanvas && node.Node is FigmaFrame) {
                 return true;
             }
 
@@ -92,7 +92,7 @@ namespace FigmaSharp.Services
 				return new FigmaNode[0];
 			}
 
-			if (node.Node.IsDialogParentContainer (NativeControlType.WindowStandard)) {
+			if (node.Node.IsDialogParentContainer (NativeControlType.Window)) {
 				if (node.Node is IFigmaNodeContainer nodeContainer) {
 					var item = nodeContainer.children.FirstOrDefault (s => s.IsNodeWindowContent ());
 					if (item != null && item is IFigmaNodeContainer children) {
