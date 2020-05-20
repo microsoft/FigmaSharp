a11e4e6dbaf4da961a95129d867bd960b1417745
Hylke Bons <hi@planetpeanut.uk>
2020-05-21 00:46:38 +0200

    cocoa: CheckBox

---
+++ FigmaSharp.NativeControls/FigmaSharp.NativeControls.Cocoa/Converters/CheckBoxConverter.cs
@@ -0,0 +1,146 @@
+﻿// Authors:
+//   Jose Medrano <josmed@microsoft.com>
+//   Hylke Bons <hylbo@microsoft.com>
+//
+// Copyright (C) 2020 Microsoft, Corp
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Linq;
+using System.Text;
+
+using AppKit;
+
+using FigmaSharp.Cocoa;
+using FigmaSharp.Models;
+using FigmaSharp.Services;
+using FigmaSharp.Views;
+using FigmaSharp.Views.Cocoa;
+
+namespace FigmaSharp.NativeControls.Cocoa
+{
+    public class CheckBoxConverter : CocoaConverter
+    {
+        public override Type GetControlType(FigmaNode currentNode) => typeof(NSButton);
+
+        public override bool CanConvert(FigmaNode currentNode)
+        {
+            return currentNode.TryGetNativeControlType(out var controlType) &&
+                controlType == NativeControlType.CheckBox;
+        }
+
+
+        protected override IView OnConvertToView (FigmaNode currentNode, ProcessedNode parentNode, FigmaRendererService rendererService)
+        {
+            var frame = (FigmaFrame)currentNode;
+
+            var checkBox = new NSButton();
+            checkBox.SetButtonType(NSButtonType.Switch);
+
+            FigmaText text = frame.children
+                  .OfType<FigmaText>()
+                  .FirstOrDefault(s => s.name == ComponentString.TITLE);
+
+            if (text != null)
+                checkBox.Title = text.characters;
+
+            frame.TryGetNativeControlVariant(out var controlVariant);
+
+            checkBox.ControlSize = GetNSControlSize(controlVariant);
+            checkBox.Font = GetNSFont(controlVariant, text);
+
+            FigmaGroup group = frame.children
+                .OfType<FigmaGroup>()
+                .FirstOrDefault(s => s.name.In(ComponentString.STATE_ON,
+                                               ComponentString.STATE_OFF,
+                                               ComponentString.STATE_MIXED) && s.visible);
+
+            if (group != null)
+            {
+                if (group.name == ComponentString.STATE_ON)
+                    checkBox.State = NSCellStateValue.On;
+
+                if (group.name == ComponentString.STATE_OFF)
+                    checkBox.State = NSCellStateValue.Off;
+
+                if (group.name == ComponentString.STATE_MIXED)
+                {
+                    checkBox.AllowsMixedState = true;
+                    checkBox.State = NSCellStateValue.Mixed;
+                }
+            }
+
+            return new View(checkBox);
+        }
+
+
+        protected override StringBuilder OnConvertToCode (FigmaCodeNode currentNode, FigmaCodeNode parentNode, FigmaCodeRendererService rendererService)
+        {
+            var code = new StringBuilder ();
+            string name = FigmaSharp.Resources.Ids.Conversion.NameIdentifier;
+
+            var frame = (FigmaFrame)currentNode.Node;
+            currentNode.Node.TryGetNativeControlType(out NativeControlType controlType);
+            currentNode.Node.TryGetNativeControlVariant(out NativeControlVariant controlVariant);
+            
+            if (rendererService.NeedsRenderConstructor (currentNode, parentNode))
+                code.WriteConstructor (name, GetControlType(currentNode.Node), rendererService.NodeRendersVar (currentNode, parentNode));
+
+            code.WriteMethod (name, nameof (NSButton.SetButtonType), NSButtonType.Switch);
+
+            code.WriteEquality(name, nameof(NSButton.ControlSize), GetNSControlSize(controlVariant));
+            code.WriteEquality(name, nameof(NSSegmentedControl.Font), GetNSFontName(controlVariant));
+
+            FigmaText text = frame.children
+                .OfType<FigmaText> ()
+                .FirstOrDefault ();
+
+            if (text != null) {
+                var labelTranslated = NativeControlHelper.GetTranslatableString(text.characters, rendererService.CurrentRendererOptions.TranslateLabels);
+
+                code.WriteEquality(name, nameof(NSButton.Title), labelTranslated,
+                    inQuotes: !rendererService.CurrentRendererOptions.TranslateLabels);
+            }
+            
+            FigmaGroup group = frame.children
+                .OfType<FigmaGroup> ()
+                .FirstOrDefault(s => s.name.In(ComponentString.STATE_ON,
+                                               ComponentString.STATE_OFF,
+                                               ComponentString.STATE_MIXED) && s.visible);
+
+            if (group != null) {
+                if (group.name == ComponentString.STATE_ON)
+                    code.WriteEquality (name, nameof (NSButton.State), NSCellStateValue.On);
+
+                if (group.name == ComponentString.STATE_OFF)
+                    code.WriteEquality(name, nameof(NSButton.State), NSCellStateValue.Off);
+
+                if (group.name == ComponentString.STATE_MIXED)
+                {
+                    code.WriteEquality(name, nameof(NSButton.AllowsMixedState), true);
+                    code.WriteEquality(name, nameof(NSButton.State), NSCellStateValue.Mixed);
+                }
+            }
+
+            return code;
+        }
+    }
+}
