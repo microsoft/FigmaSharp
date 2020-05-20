ead469e922d50d2f6dd227ef298a8baaaf214cae
Hylke Bons <hi@planetpeanut.uk>
2020-05-21 00:51:39 +0200

    cocoa: Stepper

--- FigmaSharp.NativeControls/FigmaSharp.NativeControls.Cocoa/Converters/StepperConverter.cs
+++ FigmaSharp.NativeControls/FigmaSharp.NativeControls.Cocoa/Converters/StepperConverter.cs
@@ -1,31 +1,29 @@
-﻿/* 
- * CustomTextFieldConverter.cs
- * 
- * Author:
- *   Jose Medrano <josmed@microsoft.com>
- *
- * Copyright (C) 2018 Microsoft, Corp
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to permit
- * persons to whom the Software is furnished to do so, subject to the
- * following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
- * NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+﻿// Authors:
+//   Jose Medrano <josmed@microsoft.com>
+//   Hylke Bons <hylbo@microsoft.com>
+//
+// Copyright (C) 2020 Microsoft, Corp
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+using System;
 using System.Text;
 
 using AppKit;
@@ -35,71 +33,51 @@ using FigmaSharp.Models;
 using FigmaSharp.Services;
 using FigmaSharp.Views;
 using FigmaSharp.Views.Cocoa;
-using FigmaSharp.Views.Native.Cocoa;
-using FigmaSharp.NativeControls;
-using System;
 
 namespace FigmaSharp.NativeControls.Cocoa
 {
-	public class StepperConverter : FigmaNativeControlConverter
+	public class StepperConverter : CocoaConverter
 	{
-		public override Type GetControlType(FigmaNode currentNode)
+		public override Type GetControlType(FigmaNode currentNode) => typeof(NSStepper);
+
+        public override bool CanSetAccessibilityLabel => false;
+
+
+        public override bool CanConvert(FigmaNode currentNode)
 		{
-			return typeof(NSStepper);
+			return currentNode.TryGetNativeControlType(out var controlType) &&
+                controlType == NativeControlType.Stepper;
 		}
 
-		public override bool CanConvert(FigmaNode currentNode)
+
+		protected override IView OnConvertToView (FigmaNode currentNode, ProcessedNode parentNode, FigmaRendererService rendererService)
 		{
-			return currentNode.TryGetNativeControlType(out var value) && value == NativeControlType.Stepper;
+			var stepper = new NSStepper();
+
+			var frame = (FigmaFrame)currentNode;
+			frame.TryGetNativeControlVariant(out var controlVariant);
+
+			stepper.ControlSize = GetNSControlSize(controlVariant);
+
+			return new View(stepper);
 		}
 
-		protected override IView OnConvertToView (FigmaNode currentNode, ProcessedNode parent, FigmaRendererService rendererService)
-		{
-			var instance = (FigmaFrameEntity)currentNode;
-			var view = new Stepper ();
-			var nativeView = (FNSStepper)view.NativeObject;
-			nativeView.Configure (instance);
-
-			instance.TryGetNativeControlComponentType (out var controlType);
-			switch (controlType) {
-				case NativeControlComponentType.StepperSmall:
-				case NativeControlComponentType.StepperSmallDark:
-					nativeView.ControlSize = NSControlSize.Small;
-					break;
-				case NativeControlComponentType.StepperStandard:
-				case NativeControlComponentType.StepperStandardDark:
-					nativeView.ControlSize = NSControlSize.Regular;
-					break;
-			}
-
-			return view;
-		}
 
 		protected override StringBuilder OnConvertToCode(FigmaCodeNode currentNode, FigmaCodeNode parentNode, FigmaCodeRendererService rendererService)
 		{
-			var figmaInstance = (FigmaFrameEntity)currentNode.Node;
-			var builder = new StringBuilder ();
-			string name = currentNode.Name;
+			var code = new StringBuilder();
+			string name = FigmaSharp.Resources.Ids.Conversion.NameIdentifier;
 
-			if (rendererService.NeedsRenderConstructor (currentNode, parentNode))
-				builder.WriteConstructor (name, GetControlType(currentNode.Node), rendererService.NodeRendersVar(currentNode, parentNode));
+			var frame = (FigmaFrame)currentNode.Node;
+			currentNode.Node.TryGetNativeControlType(out NativeControlType controlType);
+			currentNode.Node.TryGetNativeControlVariant(out NativeControlVariant controlVariant);
 
-			builder.Configure (figmaInstance, name);
+			if (rendererService.NeedsRenderConstructor(currentNode, parentNode))
+				code.WriteConstructor(name, GetControlType(currentNode.Node), rendererService.NodeRendersVar(currentNode, parentNode));
 
-			figmaInstance.TryGetNativeControlComponentType (out var controlType);
-			switch (controlType) {
-				case NativeControlComponentType.StepperSmall:
-				case NativeControlComponentType.StepperSmallDark:
-					builder.WriteEquality (name, nameof (NSStepper.ControlSize), NSControlSize.Small);
-					break;
-				case NativeControlComponentType.StepperStandard:
-				case NativeControlComponentType.StepperStandardDark:
-					builder.WriteEquality (name, nameof (NSStepper.ControlSize), NSControlSize.Regular);
-					break;
-			}
+			code.WriteEquality(name, nameof(NSButton.ControlSize), GetNSControlSize(controlVariant));
 
-			return builder;
+			return code;
 		}
-
 	}
 }
