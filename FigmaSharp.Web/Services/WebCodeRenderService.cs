2fc734541c7524f3c55a306beb5bad47979d5208
jmedrano <josmed@microsoft.com>
2020-06-22 13:14:51 +0200

    [Web] WebCodeRenderService now inherits from RenderService

--- FigmaSharp.Web/Services/WebCodeRenderService.cs
+++ FigmaSharp.Web/Services/WebCodeRenderService.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using FigmaSharp.Converters;
 using FigmaSharp.PropertyConfigure;
 using FigmaSharp.Services;
 
 namespace FigmaSharp.Web.Services
 {
-    public class WebCodeRenderService : CodeRenderService
-    {
-        public WebCodeRenderService(INodeProvider figmaProvider, NodeConverter[] figmaViewConverters, CodePropertyConfigureBase codePropertyConverter, ICodeNameService codeNameService = null) : base(figmaProvider, figmaViewConverters, codePropertyConverter, codeNameService)
-        {
-        }
-    }
+
+    public class WebCodeRenderService : RenderService
+	{
+		readonly internal List<CodeNode> Nodes = new List<CodeNode>();
+
+		internal CodeRenderServiceOptions CurrentRendererOptions { get; set; }
+
+		public WebCodeRenderService(INodeProvider figmaProvider, NodeConverter[] figmaViewConverters,
+			CodePropertyConfigureBase codePropertyConverter, ICodeNameService codeNameService = null) : base(figmaProvider, figmaViewConverters)
+		{
+		
+		}
+	}
 }
