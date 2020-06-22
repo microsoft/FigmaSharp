1691ab513bec22a85dad47fc1dcafb2a2d723c73
jmedrano <josmed@microsoft.com>
2020-06-22 13:15:23 +0200

    [Web] Adds WebNode

---
+++ FigmaSharp.Web/Services/WebNode.cs
@@ -0,0 +1,25 @@
+﻿using FigmaSharp.Models;
+
+namespace FigmaSharp.Web.Services
+{
+    public class WebNode
+    {
+        public WebNode(FigmaNode node, string name = null, bool isClass = false, WebNode parent = null)
+        {
+            Node = node;
+            Name = name;
+            IsClass = isClass;
+            Parent = parent;
+        }
+
+        public bool HasName => !string.IsNullOrEmpty(Name);
+
+        public FigmaNode Node { get; private set; }
+
+        public WebNode Parent { get; private set; }
+
+        public string Name { get; set; }
+
+        public bool IsClass { get; private set; }
+    }
+}
