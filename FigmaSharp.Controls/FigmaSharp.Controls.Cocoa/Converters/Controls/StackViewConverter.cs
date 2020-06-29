6c19a6ec1b52568d84e934c65a686bca1bc01ecf
Jose Medrano <josmed@microsoft.com>
2020-06-29 09:41:58 +0200

    [Cocoa] Moves StackView converter code to ConfigureProperty

--- FigmaSharp.Controls/FigmaSharp.Controls.Cocoa/Converters/Controls/StackViewConverter.cs
+++ FigmaSharp.Controls/FigmaSharp.Controls.Cocoa/Converters/Controls/StackViewConverter.cs
@@ -26,7 +26,8 @@ using System;
 using System.Text;
 
 using AppKit;
-
+using FigmaSharp.Cocoa;
+using FigmaSharp.Controls.Cocoa.Helpers;
 using FigmaSharp.Models;
 using FigmaSharp.Services;
 using FigmaSharp.Views;
@@ -36,6 +37,14 @@ namespace FigmaSharp.Controls.Cocoa.Converters
 {
     public class StackViewConverter : CocoaConverter
     {
+        public static class Properties
+        {
+            public static string EdgeInsets = "EdgeInsets";
+            public static string Spacing = "Spacing";
+            public static string Orientation = "Orientation";
+            public static string Distribution = "Distribution";
+        }
+
         public override Type GetControlType(FigmaNode currentNode) => typeof(NSStackView);
         public override bool ScanChildren(FigmaNode currentNode) => true;
 
@@ -53,68 +62,84 @@ namespace FigmaSharp.Controls.Cocoa.Converters
             return false;
         }
 
+        public void ConfigureProperty(string propertyName, FigmaNode node, IView view)
+        {
+            var frame = (FigmaFrame)node;
+
+            var stackView = (NSStackView)view.NativeObject;
+            if (propertyName == Properties.EdgeInsets)
+            {
+                stackView.EdgeInsets = new NSEdgeInsets(
+                   top: frame.verticalPadding,
+                   left: frame.horizontalPadding,
+                   bottom: frame.verticalPadding,
+                   right: frame.horizontalPadding
+                );
+                return;
+            }
+
+            if (propertyName == Properties.Spacing)
+            {
+                stackView.Spacing = frame.itemSpacing;
+                return;
+            }
+
+            if (propertyName == Properties.Orientation)
+            {
+                stackView.Orientation = frame.LayoutMode == FigmaLayoutMode.Horizontal ?
+               NSUserInterfaceLayoutOrientation.Horizontal : NSUserInterfaceLayoutOrientation.Vertical;
+                return;
+            }
+
+            if (propertyName == Properties.Distribution)
+            {
+                stackView.Distribution = NSStackViewDistribution.FillEqually;
+                return;
+            }
+        }
 
         protected override IView OnConvertToView (FigmaNode currentNode, ViewNode parentNode, ViewRenderService rendererService)
         {
-            var frame = (FigmaFrame)currentNode;
-            var stackView = new NSStackView();
+            var view = new View(new NSStackView());
 
-            stackView.EdgeInsets = new NSEdgeInsets(
-                top:    frame.verticalPadding,
-                left:   frame.horizontalPadding,
-                bottom: frame.verticalPadding,
-                right:  frame.horizontalPadding);
+            ConfigureProperty(Properties.EdgeInsets, currentNode, view);
+            ConfigureProperty(Properties.Spacing, currentNode, view);
+            ConfigureProperty(Properties.Orientation, currentNode, view);
+            ConfigureProperty(Properties.Distribution, currentNode, view);
 
-            stackView.Spacing = frame.itemSpacing;
-
-            stackView.Orientation = frame.LayoutMode == FigmaLayoutMode.Horizontal ?
-                NSUserInterfaceLayoutOrientation.Horizontal : NSUserInterfaceLayoutOrientation.Vertical;
-            stackView.Distribution = NSStackViewDistribution.FillEqually;
-
-            return new View(stackView);
+            return view;
         }
-
 
         protected override StringBuilder OnConvertToCode(CodeNode currentNode, CodeNode parentNode, CodeRenderService rendererService)
         {
             var code = new StringBuilder();
-            string name = FigmaSharp.Resources.Ids.Conversion.NameIdentifier;
+
+            string identifier = Resources.Ids.Conversion.NameIdentifier;
 
             var frame = (FigmaFrame)currentNode.Node;
 
-            /*
             currentNode.Node.TryGetNativeControlType(out FigmaControlType controlType);
             currentNode.Node.TryGetNativeControlVariant(out NativeControlVariant controlVariant);
 
             if (rendererService.NeedsRenderConstructor(currentNode, parentNode))
-                code.WriteConstructor(name, GetControlType(currentNode.Node), rendererService.NodeRendersVar(currentNode, parentNode));
+                code.WriteConstructor(identifier, GetControlType(currentNode.Node), rendererService.NodeRendersVar(currentNode, parentNode));
 
-            var itemNodes = frame.FirstChild(s => s.name == ComponentString.ITEMS);
+            code.WriteEquality(identifier, nameof(NSButton.ControlSize), ViewHelper.GetNSControlSize(controlVariant));
 
-            if (itemNodes == null)
-                return null;
+            var edgeInsets = typeof(NSEdgeInsets).GetConstructor(
+                frame.verticalPadding.ToString (),
+                frame.horizontalPadding.ToString(),
+                frame.verticalPadding.ToString(),
+                frame.horizontalPadding.ToString());
 
-            code.AppendLine();
-            code.AppendLine($"{ name }.{ nameof(NSTabView.SetItems) }(");
-            code.AppendLine($"\tnew { typeof(NSTabViewItem[]) }");
-            code.AppendLine("\t{");
+            code.WriteEquality(identifier, nameof(NSStackView.Spacing), edgeInsets);
 
-            foreach (FigmaNode tabNode in itemNodes.GetChildren(t => t.visible, reverseChildren: true))
-            {
-                var firstChild = tabNode.FirstChild(s => s.name.In(ComponentString.STATE_REGULAR, ComponentString.STATE_SELECTED) && s.visible);
+            var orientation = frame.LayoutMode == FigmaLayoutMode.Horizontal ?
+                NSUserInterfaceLayoutOrientation.Horizontal : NSUserInterfaceLayoutOrientation.Vertical;
 
-                if (firstChild != null)
-                {
-                    FigmaText text = firstChild.FirstChild(s => s.name == ComponentString.TITLE) as FigmaText;
-
-                    if (text != null)
-                        code.AppendLine($"\t\tnew {typeof(NSTabViewItem)}() {{ {nameof(NSTabViewItem.Label)} = \"{text.characters}\" }},");
-                }
-            }
-
-            code.AppendLine("\t}");
-            code.AppendLine(");");
-            */
+            code.WriteEquality(identifier, nameof(NSStackView.Orientation), orientation.GetFullName ());
+            code.WriteEquality(identifier, nameof(NSStackView.Distribution), NSStackViewDistribution.FillEqually.GetFullName());
+           
             return code;
         }
     }
