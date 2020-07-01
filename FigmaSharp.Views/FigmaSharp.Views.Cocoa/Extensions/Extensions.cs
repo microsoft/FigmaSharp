253be31c6cc6145212aad1c101b8f37319da8f22
Jose Medrano <josmed@microsoft.com>
2020-07-01 20:20:19 +0200

    [Views.Cocoa] Moves CGPathExtensions extensions to a separate file

--- FigmaSharp.Views/FigmaSharp.Views.Cocoa/Extensions/Extensions.cs
+++ FigmaSharp.Views/FigmaSharp.Views.Cocoa/Extensions/Extensions.cs
@@ -5,7 +5,7 @@ using FigmaSharp.Views;
 
 namespace FigmaSharp
 {
-	public static class Extensions
+    public static class ConversionExtensions
 	{
 		public static CGSize ToCGSize (this Size size)
 		{
@@ -69,42 +69,5 @@ namespace FigmaSharp
 			return NSUserInterfaceLayoutOrientation.Vertical;
 		}
 
-		//TODO: we should move this to a shared place
-		public static CGPath ToCGPath (this NSBezierPath path)
-		{
-			var numElements = path.ElementCount;
-			if (numElements == 0) {
-				return null;
-			}
-
-			CGPath result = new CGPath ();
-			bool didClosePath = true;
-
-
-			for (int i = 0; i < numElements; i++) {
-				CGPoint[] points;
-				var element = path.ElementAt (i, out points);
-				if (element == NSBezierPathElement.MoveTo) {
-					result.MoveToPoint (points[0].X, points[0].Y);
-				} else if (element == NSBezierPathElement.LineTo) {
-					result.AddLineToPoint (points[0].X, points[0].Y);
-					didClosePath = false;
-
-				} else if (element == NSBezierPathElement.CurveTo) {
-					result.AddCurveToPoint (points[0].X, points[0].Y,
-											points[1].X, points[1].Y,
-											points[2].X, points[2].Y);
-					didClosePath = false;
-				} else if (element == NSBezierPathElement.ClosePath) {
-					result.CloseSubpath ();
-				}
-			}
-
-			// Be sure the path is closed or Quartz may not do valid hit detection.
-			if (!didClosePath) {
-				result.CloseSubpath ();
-			}
-			return result;
-		}
 	}
 }
---
+++ FigmaSharp.Views/FigmaSharp.Views.Cocoa/Extensions/PathExtensions.cs
@@ -0,0 +1,55 @@
+﻿using CoreGraphics;
+
+namespace AppKit
+{
+    public static class CGPathExtensions
+    {
+        //TODO: we should move this to a shared place
+        public static CGPath ToCGPath(this NSBezierPath path)
+        {
+            var numElements = path.ElementCount;
+            if (numElements == 0)
+            {
+                return null;
+            }
+
+            CGPath result = new CGPath();
+            bool didClosePath = true;
+
+
+            for (int i = 0; i < numElements; i++)
+            {
+                CGPoint[] points;
+                var element = path.ElementAt(i, out points);
+                if (element == NSBezierPathElement.MoveTo)
+                {
+                    result.MoveToPoint(points[0].X, points[0].Y);
+                }
+                else if (element == NSBezierPathElement.LineTo)
+                {
+                    result.AddLineToPoint(points[0].X, points[0].Y);
+                    didClosePath = false;
+
+                }
+                else if (element == NSBezierPathElement.CurveTo)
+                {
+                    result.AddCurveToPoint(points[0].X, points[0].Y,
+                                            points[1].X, points[1].Y,
+                                            points[2].X, points[2].Y);
+                    didClosePath = false;
+                }
+                else if (element == NSBezierPathElement.ClosePath)
+                {
+                    result.CloseSubpath();
+                }
+            }
+
+            // Be sure the path is closed or Quartz may not do valid hit detection.
+            if (!didClosePath)
+            {
+                result.CloseSubpath();
+            }
+            return result;
+        }
+    }
+}
