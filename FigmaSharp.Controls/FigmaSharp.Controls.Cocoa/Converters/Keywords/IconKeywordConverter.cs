b120302433a820dd116d4b3ebe39d14f58ac1185
Hylke Bons <hi@planetpeanut.uk>
2021-04-09 18:46:06 +0200

    cocoa icons: Fix icon sizing issue

--- FigmaSharp.Controls/FigmaSharp.Controls.Cocoa/Converters/Keywords/IconKeywordConverter.cs
+++ FigmaSharp.Controls/FigmaSharp.Controls.Cocoa/Converters/Keywords/IconKeywordConverter.cs
@@ -58,6 +58,7 @@ namespace FigmaSharp.Controls.Cocoa
 
 			NSImageView iconView = new NSImageView();
 			iconView.Image = NSImage.ImageNamed(iconName);
+			iconView.ImageScaling = NSImageScale.ProportionallyUpOrDown;
 			iconView.Image.Size = new CGSize(frame.absoluteBoundingBox.Width, frame.absoluteBoundingBox.Height);
 
 			return new View(iconView);
@@ -77,6 +78,8 @@ namespace FigmaSharp.Controls.Cocoa
 
 			code.WritePropertyEquality(name, nameof(NSImageView.Image),
 				$"{ typeof(NSImage) }.{ nameof(NSImage.ImageNamed) }(\"{ iconName }\")");
+
+			code.WritePropertyEquality(name, nameof(NSImageView.ImageScaling), NSImageScaling.ProportionallyUpOrDown);
 
 			code.WritePropertyEquality(name, $"{ nameof(NSImageView.Image) }.{ nameof(NSImageView.Image.Size) }",
 				$"new { typeof(CGSize) }({ frame.absoluteBoundingBox.Width }, { frame.absoluteBoundingBox.Height })");
