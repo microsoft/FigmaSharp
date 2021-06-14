3ee3772d015e7555eb93c473a0ec8ae1837e4f9e
Jose Medrano <josmed@microsoft.com>
2021-06-14 14:00:27 +0200

    Bumps extension version

--- tools/MonoDevelop.Figma/Properties/AddinInfo.cs
+++ tools/MonoDevelop.Figma/Properties/AddinInfo.cs
@@ -28,7 +28,7 @@ using Mono.Addins.Description;
 [assembly: Addin (
 	"MonoDevelop.Figma",
 	Namespace = "Xamarin",
-	Version = "0.8.3"
+	Version = "0.8.4"
 )]
 
 [assembly: AddinName ("FigmaSharp")]
